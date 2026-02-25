d24cd062f385de639dfb67c6fa7a21c29e34cd87
LuisDentler <luis.dentler06@gmail.com>
2026-02-25 14:43:05 +0000

    Update Pseudocode for output_statistics_timestep

--- statistics.c
+++ statistics.c
@@ -37,10 +37,11 @@ FUNKTION init_statistik(dateiname):
     log_datei = öffne_datei_zum_schreiben(dateiname)
     WENN log_datei IST NULL:
         GIB -1 ZURÜCK
-        
+
     // Variablen zurücksetzen
     gesamt_zeit        = 0
     gesamt_belegung    = 0
+    gesamt_wartezeit   = 0
     gesamt_wartequeue  = 0
     max_queue_laenge   = 0
     volle_belegung     = 0
@@ -53,4 +54,37 @@ FUNKTION init_statistik(dateiname):
 int init_statistics(const char *filename)
 {
  /*spätere implementierung*/   
+}
+
+
+/*---------------------------------------------------------------*/
+/* Output statistic time step
+/*---------------------------------------------------------------*/
+/*
+PSEUDOCODE
+FUNKTION void output_timestep_statistics(aktueller_schritt, belegung, länge_warteschlange, anz_autos_verlassen_parkhaus, wartezeit_geparkter_autos);
+    gesamt_zeit += 1
+    gesamt_belegung += belegung
+    gesamt_wartezeit += wartezeit_geparkter_autos
+    gesamt_wartequeue += warteschlange
+
+    WENN warteschlange > max_queue_laenge:
+        max_queue_laenge = warteschlange
+
+    WENN belegung IST VOLLE_KAPAZITÄT:
+        volle_belegung += 1
+    
+    /*Berechnung durchschnitliche Wartezeit(Gesamtwartezeit aller bisher geparkten autos/ Anzahl aller geparkten autos (gesamt_belegung))
+    durchschnitt_wartezeit = gesamt_wartezeit/anz_autos_parken
+
+    gesamt_durchlauf += anz_autos_verlassen_parkhaus
+    
+    print("Aktuelle Belegung", belegung | "Warteschlange", länge_warteschlange| "Durchschn. Wartezeit", durchschnitt_wartezeit| "Vollbelegungs Ticks", volle_belegung| "Gesamt-Durchsatz",gesamt_durchlauf)
+
+*/
+/*---------------------------------------------------------------*/
+
+void output_timestep_statistics(int current_step, int occupancy, int queue_len)
+{
+    /*spätere Implementierung*/
 }
