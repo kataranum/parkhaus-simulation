182d1e992e1c529ea653ae90f04134c5730f41e2
LuisDentler <luis.dentler06@gmail.com>
2026-02-25 13:31:40 +0000

    Add Pseudocode for init_statistics().

--- statistics.c
+++ statistics.c
@@ -25,3 +25,32 @@ static int max_queue_length = 0;
 
 /* Full occupancy */
 static int full_occupancy_steps = 0;
+
+/*---------------------------------------------------------------*/
+/* Initialization 
+/*---------------------------------------------------------------*/
+/*
+PSEUDOCODE
+FUNKTION init_statistik(dateiname):
+    WENN dateiname IST NULL:
+        GIB -1 ZURÜCK
+    log_datei = öffne_datei_zum_schreiben(dateiname)
+    WENN log_datei IST NULL:
+        GIB -1 ZURÜCK
+        
+    // Variablen zurücksetzen
+    gesamt_zeit        = 0
+    gesamt_belegung    = 0
+    gesamt_wartequeue  = 0
+    max_queue_laenge   = 0
+    volle_belegung     = 0
+
+    schreibe_log("==== PARKHAUS-SIMULATION LOG ====")
+    GIB 0 ZURÜCK
+*/
+/*---------------------------------------------------------------*/
+
+int init_statistics(const char *filename)
+{
+ /*spätere implementierung*/   
+}
