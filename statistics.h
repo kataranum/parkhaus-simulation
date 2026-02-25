8c86c3b391edab55e9df161c7424bd3aca005cbe
LuisDentler <luis.dentler06@gmail.com>
2026-02-25 11:15:38 +0000

    Add statistics_update_timestep function header.

--- statistics.h
+++ statistics.h
@@ -23,5 +23,20 @@
 int init_statistics(const char *filename);
 
 
+/*---------------------------------------------------------------*/
+/* Time step based updates                                                */
+/*---------------------------------------------------------------*/
+/**
+ * @brief Update statistics that are evaluated per simulation step
+ *
+ * This function is called exactly once per simulation time step.
+ * It updates values related to occupancy, queue length and
+ * full-capacity detection.
+ *
+ * @param[in] occupied_spaces  Number of currently occupied parking spaces
+ * @param[in] queue_length     Current number of vehicles in the waiting queue
+ * @param[in] total_capacity   Total number of parking spaces
+ */
+void statistics_update_timestep(int occupied_spaces, int queue_length, int total_capacity);
 
 #endif /* STATISTICS_H */
