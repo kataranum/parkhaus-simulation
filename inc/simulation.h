397d109f59065065b2cfe25dd89dd7d8cfa223da
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 12:19:22 +0100

    Document park_waiting_cars()

--- inc/simulation.h
+++ inc/simulation.h
@@ -18,11 +18,22 @@ struct Car;
  * This function also makes calls to statistics functions to mark the departure
  * in statistics.
  *
- * @param current_step 
- * @param parking_lot 
+ * @param current_step[in] Current simulation time step 
+ * @param parking_lot[in/out] Current simulation parking lot
  */
 void remove_due_cars(unsigned int current_step, ParkingLot parking_lot);
 
+/**
+ * @brief Move cars from the queue in the parking lot until the lot is full
+ * 
+ * The cars in `waiting_cars` are waiting for free spots in `parking_lot`. This
+ * function will move cars from the queue into the lot until the lot is filled,
+ * or the queue becomes empty. If the lot is full, the remaining cars in
+ * `waiting_cars` remain in queue until next time step (or longer).
+ *
+ * @param parking_lot[in/out] Current simulation parking lot
+ * @param waiting_cars[in/out] Current simulation car queue
+ */
 void park_waiting_cars(ParkingLot parking_lot, QueueCars waiting_cars);
 
 void get_new_cars_arriving(QueueCars waiting_cars);
