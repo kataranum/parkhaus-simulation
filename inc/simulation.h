228be7497c9298bb80037fecbd3c531d9cb52d99
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 12:07:22 +0100

    Define header for remove_due_cars()

--- inc/simulation.h
+++ inc/simulation.h
@@ -1,6 +1,9 @@
 #ifndef H_SIMULATION
 #define H_SIMULATION
 
+// TODO: Define proper array for as parking_lot
+typedef void* ParkingLot;
 
+void remove_due_cars(unsigned int current_step, ParkingLot parking_lot);
 
 #endif
