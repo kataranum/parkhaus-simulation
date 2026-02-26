56b596cbf3778fa8ab86e1a48d3e3de1a82b0253
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 14:46:17 +0100

    Write pseudocode for room_available

--- src/simulation.c
+++ src/simulation.c
@@ -84,6 +84,23 @@ FOR i IN 0 TO parking_lot.length:
     car = parking_lot.array[i];
 
     IF is_empty(car):
+        RETURN True;
+    END IF
+END FOR
+
+RETURN False;
+```
+*/
+int room_available(const ParkingLot *p_parking_lot) {
+    // TODO
+}
+
+/*
+```PSEUDOCODE
+FOR i IN 0 TO parking_lot.length:
+    car = parking_lot.array[i];
+
+    IF is_empty(car):
         RETURN i;
     END IF
 END FOR
