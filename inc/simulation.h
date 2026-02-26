22dbd5431673be15f81a896d88ef93474a22edb6
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 12:12:25 +0100

    Define headers for other functions

--- inc/simulation.h
+++ inc/simulation.h
@@ -15,6 +15,11 @@ void park_waiting_cars(ParkingLot parking_lot, QueueCars waiting_cars);
 
 void get_new_cars_arriving(QueueCars waiting_cars);
 
+int room_available(ParkingLot parking_lot);
+
 Car init_new_car();
+int get_unique_id();
+
+float frand();
 
 #endif
