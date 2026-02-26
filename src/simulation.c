3fafb070efbc95a2810657334a72f1a87652b62f
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 14:57:08 +0100

    Move init_car pseudocode to source as well

--- src/simulation.c
+++ src/simulation.c
@@ -116,6 +116,21 @@ int find_empty_space(const ParkingLot *p_parking_lot) {
 
 /*
 ```PSEUDOCODE
+Car car;
+
+car.id = get_unique_id();
+car.time_arrival = 0;
+car.time_park_duration = random_park_duration();
+
+RETURN car
+```
+*/
+Car init_new_car(InputParams params) {
+    // TODO
+}
+
+/*
+```PSEUDOCODE
 RETURN (rand() % (params.max_park_duration - 1)) + 1
 ```
 */
