6fd41fee61285defad3200097f64925c6cc96a18
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 12:08:41 +0100

    Define header for park_waiting_cars()

--- inc/simulation.h
+++ inc/simulation.h
@@ -3,7 +3,11 @@
 
 // TODO: Define proper array for as parking_lot
 typedef void* ParkingLot;
+// TODO: Define proper Queue struct for cars
+typedef void* QueueCars;
 
 void remove_due_cars(unsigned int current_step, ParkingLot parking_lot);
+
+void park_waiting_cars(ParkingLot parking_lot, QueueCars waiting_cars);
 
 #endif
