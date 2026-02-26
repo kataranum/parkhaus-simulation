ba74f4f362ba8ca1ce510d4e02a21a2ef03d4308
Kevin Müller <muellerk2006@gmail.com>
2026-02-26 12:16:16 +0100

    Document remove_due_cars

--- inc/simulation.h
+++ inc/simulation.h
@@ -9,6 +9,18 @@ typedef void* QueueCars;
 // TODO: Replace placeholder with proper struct definition
 struct Car;
 
+/**
+ * @brief Have cars over their max park time leave the parking lot
+ * 
+ * Every car has a set parking duration. After this time passed, the car must
+ * leave the parking lot. This function takes care of that.
+ *
+ * This function also makes calls to statistics functions to mark the departure
+ * in statistics.
+ *
+ * @param current_step 
+ * @param parking_lot 
+ */
 void remove_due_cars(unsigned int current_step, ParkingLot parking_lot);
 
 void park_waiting_cars(ParkingLot parking_lot, QueueCars waiting_cars);
