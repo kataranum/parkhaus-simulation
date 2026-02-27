b0fc507ba76ac8a00c1c02990d8dcb14e5629798
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-02-27 10:17:34 +0100

    Copy pseudocode for parking lot

---
+++ src/parking_lot.c
@@ -0,0 +1,35 @@
+#include "../inc/parking_lot.h"
+
+/*
+```PSEUDOCODE
+FOR i IN 0 TO parking_lot.length:
+    car = parking_lot.array[i];
+
+    IF is_empty(car):
+        RETURN True;
+    END IF
+END FOR
+
+RETURN False;
+```
+*/
+int room_available(const ParkingLot *p_parking_lot) {
+    // TODO
+}
+
+/*
+```PSEUDOCODE
+FOR i IN 0 TO parking_lot.length:
+    car = parking_lot.array[i];
+
+    IF is_empty(car):
+        RETURN i;
+    END IF
+END FOR
+
+RETURN -1
+```
+*/
+int find_empty_space(const ParkingLot *p_parking_lot) {
+    // TODO
+}
