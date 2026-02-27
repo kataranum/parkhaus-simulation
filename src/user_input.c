b2e6e705af961c3b439be291127a8f5f3a363a70
Kevin Müller <muellerk2006@gmail.com>
2026-02-27 17:05:27 +0100

    Write pseudocode for get_user_input

--- src/user_input.c
+++ src/user_input.c
@@ -1,1 +1,30 @@
 #include "../inc/user_input.h"
+
+/*
+```PSEUDOCODE
+InputParams params;
+
+PRINT "Please input number of parking spaces"
+input_valid_uint(&params.park_num_spaces);
+
+PRINT "Please input maximum number of timesteps that cars will park for"
+input_valid_uint(&params.park_max_time);
+
+PRINT "Please enter a percentage chance of cars arriving on a new timestep"
+float percentage;
+input_valid_float(&percentage);
+assert_float_range(percentage, 0.0, 100.0);
+params.park_chance_arrive = percentage / 100.0;
+
+PRINT "Please input how many timesteps should be simulated"
+input_valid_uint(&params.total_time_steps);
+
+PRINT "Enter seed (leave empty for random seed)"
+input_valid_ulong(&params.rng_seed);
+
+RETURN params
+```
+*/
+InputParams get_user_input(void) {
+    // TODO
+}
