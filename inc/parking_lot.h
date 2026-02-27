4a5fd487bf6ec532a443f4d0c091e076744b88af
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-02-27 10:15:19 +0100

    Copy pseudocode from other branch

--- inc/parking_lot.h
+++ inc/parking_lot.h
@@ -10,4 +10,23 @@ typedef struct
     unsigned int length;
 }ParkingLot;
 
+/**
+ * @brief Determine whether there are free spots in `parking_lot`
+ * 
+ * @param parking_lot[in] Given parking lot
+ * @return int (boolean) whether there is at least one free spot
+ */
+int room_available(const ParkingLot *p_parking_lot);
+
+/**
+ * @brief Find any empty free spot in `parking_lot`
+ * 
+ * In case there are no free spots, return -1. Otherwise return the index of
+ * a free spot.
+ *
+ * @param parking_lot[in] Given parking lot 
+ * @return int Index to a free parking space
+ */
+int find_empty_space(const ParkingLot *p_parking_lot);
+
 #endif
