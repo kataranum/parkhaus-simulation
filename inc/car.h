baf37208270850ba675aaec440557cd77c901d36
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-02-27 10:06:39 +0100

    Copy car functions from other branch

--- inc/car.h
+++ inc/car.h
@@ -14,4 +14,36 @@ typedef struct
     int time_park_duration;
 }Car;
 
+/**
+ * @brief Initialize a new car with random ID and park duration
+ * 
+ * @param params[in] Input parameters
+ * @return Car Newly initialized car
+ */
+Car init_new_car(InputParams params);
+
+/**
+ * @brief Get a new unique ID for Car
+ *
+ * Should never return the same ID twice.
+ *
+ * Internally, a static variable is incremented and returned every call, so the
+ * returned value will always increase.
+ * 
+ * @return int New unique ID
+ */
+int get_unique_id();
+
+/**
+ * @brief Get a random park duration for a new car
+ *
+ * Returns a random int between 1 and MAX_PARK_TIME. The latter being the
+ * maximum allowed park time for a car defined by the input parameters
+ * 
+ * @param params[in] Input parameters
+ * @return int Number of timesteps that the car wants to park for
+ */
+int random_park_duration(InputParams params);
+
+
 #endif
