bc0f837e2dcce19814d253611e32a82220a20b3a
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-02-27 10:35:10 +0100

    Add pseudocode for missing parking_lot functions

--- src/parking_lot.c
+++ src/parking_lot.c
@@ -2,6 +2,26 @@
 
 /*
 ```PSEUDOCODE
+ParkingLot parking_lot;
+parking_lot.array = malloc(length * sizeof(Car));
+IF parking_lot.array == NULL:
+    EXIT with error code
+END IF
+parking_lot.length = length;
+RETURN parking_lot
+```
+*/
+ParkingLot init_parking_lot(unsigned int length);
+
+/*
+```PSEUDOCODE
+free(parking_lot.array);
+```
+*/
+void free_parking_lot(ParkingLot parking_lot);
+
+/*
+```PSEUDOCODE
 FOR i IN 0 TO parking_lot.length:
     car = parking_lot.array[i];
 
