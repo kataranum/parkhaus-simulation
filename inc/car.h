0aa22d77fa6a3dfda220a9818bb1f3e2cc74bf8d
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-02-27 09:34:42 +0100

    Define car struct

---
+++ inc/car.h
@@ -0,0 +1,21 @@
+#ifdef SIMULATION_H
+#define SIMULATION_H
+
+typedef struct 
+{
+    unsigned int id;
+    int time_arrival_park;
+    int time_arrival_queue;
+    int time_park_duration;
+}Car;
+
+
+
+
+
+
+
+
+
+
+#endif
