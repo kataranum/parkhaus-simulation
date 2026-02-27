e11f73ed884c0966f2b0550d176a33595bc124c0
Kevin Müller <muellerk2006@gmail.com>
2026-02-27 16:57:39 +0100

    Define InputParams

--- inc/user_input.h
+++ inc/user_input.h
@@ -1,4 +1,21 @@
 #ifndef USER_INPUT_H
 #define USER_INPUT_H
 
+/**
+ * @brief All values that come from user input
+ */
+typedef struct {
+    /** Number of spaces in the parking lot */
+    unsigned int park_num_spaces;
+    /** Maximum time a car may park for */
+    unsigned int park_max_time;
+    /** Chance that new car will arrive each timestep (valid range 0 - 1) */
+    float park_chance_arrive;
+
+    /** Number of timesteps the simulation will run for */
+    unsigned int total_time_steps;
+    /** Seed for rng functions */
+    unsigned long rng_seed;
+} InputParams;
+
 #endif
