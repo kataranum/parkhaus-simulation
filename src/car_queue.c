4551a2c69b4225769a37cb4edbb1b622146b8dfb
Kevin Müller <muellerk2006@gmail.com>
2026-03-01 21:14:49 +0100

    Create car_queue.c

---
+++ src/car_queue.c
@@ -0,0 +1,21 @@
+#include "../inc/car_queue.h"
+
+CarQueue queue_init() {
+    // TODO
+}
+
+void queue_delete(CarQueue *p_queue) {
+    // TODO
+}
+
+void enqueue(CarQueue *p_queue, Car car) {
+    // TODO
+}
+
+Car dequeue(CarQueue *p_queue) {
+    // TODO
+}
+
+int is_empty(CarQueue queue) {
+    // TODO
+}
