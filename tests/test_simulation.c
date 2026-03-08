de124084d94c8a74199b55d7dc520162bd31262b
Kevin Müller <muellerk2006@gmail.com>
2026-03-09 00:00:15 +0100

    Define default simulation parameters

--- tests/test_simulation.c
+++ tests/test_simulation.c
@@ -3,8 +3,34 @@
 #include <assert.h>
 #include <simulation.h>
 
+// default values for most unit tests
+#define PARK_NUM_SPACES 10
+#define PARK_MAX_TIME 10
+#define PARK_CHANCE_ARRIVE 0.5
+#define TOTAL_TIME_STEPS 100
+
+SimulationData get_simdata_default(void) {
+    InputParams params;
+    params.park_num_spaces = PARK_NUM_SPACES;
+    params.park_max_time = PARK_MAX_TIME;
+    params.park_chance_arrive = 0.5;
+    params.total_time_steps = TOTAL_TIME_STEPS;
+    params.rng_seed = time(NULL);
+
+    Statistics *p_stats = malloc(sizeof(p_stats));
+
+    SimulationData data;
+    data.params = params;
+    data.current_step = 0;
+    data.p_stats = p_stats;
+    data.parking_lot = init_parking_lot(PARK_NUM_SPACES);
+    data.waiting_cars = queue_init();
+
+    return data;
+}
+
 void test_remove_due_cars(void) {
-    assert(false);
+    SimulationData data;
 }
 
 void test_park_waiting_cars(void) {
