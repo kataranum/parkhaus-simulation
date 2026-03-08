22c14638cf01ecff5c1563da9ea8ac0830fcbe0f
Kevin Müller <muellerk2006@gmail.com>
2026-03-08 23:35:31 +0100

    Implement test_queue_delete()

--- tests/test_car_queue.c
+++ tests/test_car_queue.c
@@ -8,11 +8,37 @@ void test_queue_init(void) {
 
     assert(q.p_front == NULL);
     assert(q.p_back == NULL);
-    assert(q.length == NULL);
+    assert(q.length == 0);
 }
 
 void test_queue_delete(void) {
-    assert(false);
+    CarQueue q = queue_init();
+
+    // fields don't matter so don't initialize
+    Car car;
+
+    // make long queue to increase chance of SEGFAULT triggering if something
+    // wrong is going on
+    const int QUEUE_LEN = 100;
+    for (int i = 0; QUEUE_LEN; i++) {
+        enqueue(&q, car);
+    }
+
+    queue_delete(&q);
+
+    assert(q.p_front == NULL);
+    assert(q.p_back == NULL);
+    assert(q.length == 0);
+}
+
+void test_queue_delete_empty(void) {
+    CarQueue q = queue_init();
+
+    queue_delete(&q);
+
+    assert(q.p_front == NULL);
+    assert(q.p_back == NULL);
+    assert(q.length == 0);
 }
 
 void test_enqueue(void) {
@@ -30,6 +56,7 @@ void test_is_empty(void) {
 int main(void) {
     test_queue_init();
     test_queue_delete();
+    test_queue_delete_empty();
     test_enqueue();
     test_dequeue();
     test_is_empty();
