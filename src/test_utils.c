be009274a52490a8faf29f4ba9979fdcc770e4d2
Kevin Müller <muellerk2006@gmail.com>
2026-03-08 22:51:56 +0100

    Implement cmp_float() and cmp_float_epsilon()

--- src/test_utils.c
+++ src/test_utils.c
@@ -1,1 +1,12 @@
 #include <test_utils.h>
+#include <math.h>
+
+bool cmp_float(float a, float b) {
+    const float EPSILON = 0.0001;
+    return cmp_float_epsilon(a, b, EPSILON);
+}
+
+bool cmp_float_epsilon(float a, float b, float epsilon) {
+    float difference = fabs(a - b);
+    return difference < epsilon;
+}
