bba1f533e1d30e78f75eb3051b3654e05cfdb75c
Kevin Müller <muellerk2006@gmail.com>
2026-03-08 23:30:30 +0100

    Define unit test functions

--- tests/test_car_queue.c
+++ tests/test_car_queue.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 #include <car_queue.h>
 
+void test_queue_init(void) {
+    assert(false);
+}
+
+void test_queue_delete(void) {
+    assert(false);
+}
+
+void test_enqueue(void) {
+    assert(false);
+}
+
+void test_dequeue(void) {
+    assert(false);
+}
+
+void test_is_empty(void) {
+    assert(false);
+}
+
 int main(void) {
+    test_queue_init();
+    test_queue_delete();
+    test_enqueue();
+    test_dequeue();
+    test_is_empty();
+
     printf("Alle Unit Tests sind erfolgreich durchgelaufen.\n");
 }
