1a47c1b44583f4a3626a7baa2d0aceff9a6321e8
Kevin Müller <muellerk2006@gmail.com>
2026-03-08 23:53:13 +0100

    Create test functions

--- tests/test_simulation.c
+++ tests/test_simulation.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 #include <simulation.h>
+
+void test_remove_due_cars(void) {
+    assert(false);
+}
+
+void test_park_waiting_cars(void) {
+    assert(false);
+}
+
+void test_get_new_cars_arriving(void) {
+    assert(false);
+}
 
 int main(void) {
     printf("All unit tests sind erfolgreich durchgelaufen.\n");
