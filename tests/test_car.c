839bdf2b85c5eb2d8f59d813a7b0561927996a8f
Kevin Müller <muellerk2006@gmail.com>
2026-03-08 22:56:52 +0100

    Create test function for each car function

--- tests/test_car.c
+++ tests/test_car.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
 #include <car.h>
 
+void test_init(void) {
+    assert(false);
+}
+
+void test_unique_id(void) {
+    assert(false);
+}
+
+void test_random_park_duration(void) {
+    assert(false);
+}
+
+void test_is_empty(void) {
+    assert(false);
+}
+
 int main(void) {
+    test_init();
+    test_unique_id();
+    test_random_park_duration();
+    test_is_empty();
 
     printf("Alle unit tests sind erfolgreich durchgelaufen\n");
     return 0;
