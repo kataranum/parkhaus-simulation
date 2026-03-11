18a244a662a263688964bb602e8ff58a133a1126
Kevin Müller <muellerk2006@gmail.com>
2026-03-11 12:08:06 +0100

    Copy function headers to C file

--- src/parse_num.c
+++ src/parse_num.c
@@ -1,1 +1,13 @@
 #include <parse_num.h>
+
+bool parse_uint(const char *str, unsigned int *p_val) {
+
+}
+
+bool parse_ulong(const char *str, unsigned long *p_val) {
+
+}
+
+bool parse_float(const char *str, float *p_val) {
+    
+}
