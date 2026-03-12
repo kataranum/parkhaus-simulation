bf24c58d9637cff7594c1b82abe9143c6bff9d27
Kevin Müller <muellerk2006@gmail.com>
2026-03-12 14:19:19 +0100

    Run tests with given seeds

--- tests/test_util.c
+++ tests/test_util.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <util.h>
 
+const long TEST_SEEDS[] = {
+    42, 123, 987,
+};
+const int TEST_SEED_AMOUNT = sizeof(TEST_SEEDS) / sizeof(TEST_SEEDS[0]);
+
+void test_frand(long seed) {
+    #define NUM_TESTS 10000
+
+    srand(seed);
+}
+
 int main(void) {
+    for (int i = 0; i < TEST_SEED_AMOUNT; i++) {
+        test_frand(TEST_SEEDS[i]);
+    }
+
     printf("Alle Unit Tests sind erfolgreich durchgelaufen.\n");
     return 0;
 }
