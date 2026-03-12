1b05ba28dd79f509a7de4fd13429e35a8060de15
Kevin Müller <muellerk2006@gmail.com>
2026-03-12 14:40:15 +0100

    Copy parse_ulong() tests to parse_uint()

--- tests/test_parse_num.c
+++ tests/test_parse_num.c
@@ -56,12 +56,54 @@ void test_parse_invalid_ulong(void) {
 
 // test successful parsing
 void test_parse_uint(void) {
-    assert(false);
+    char *test_inputs[] = {
+        "123",
+        "5",
+        "0101",
+        "0",
+        "   456  ",
+    };
+    unsigned int expected_outputs[] = {
+        123,
+        5,
+        101,
+        0,
+        456,
+    };
+
+    const int AMOUNT_INPUTS = sizeof(test_inputs) / sizeof(test_inputs[0]);
+    const int AMOUNT_OUTPUTS = sizeof(expected_outputs) / sizeof(expected_outputs[0]);
+
+    assert(AMOUNT_INPUTS == AMOUNT_OUTPUTS);
+
+    for (int i = 0; i < AMOUNT_INPUTS; i++) {
+        unsigned int output = 0;
+        bool success = parse_uint(test_inputs[i], &output);
+
+        assert(success);
+        assert(output == expected_outputs[i]);
+    }
 }
 
 // test graceful failure on invalid strings
 void test_parse_invalid_uint(void) {
-    assert(false);
+    char *test_inputs[] = {
+        "asdf",
+        "-5",
+        "",
+        "5a",
+        "10.0",
+    };
+
+    const int AMOUNT_INPUTS = sizeof(test_inputs) / sizeof(test_inputs[0]);
+
+    for (int i = 0; i < AMOUNT_INPUTS; i++) {
+        unsigned int output = 37;
+        bool success = parse_uint(test_inputs[i], &output);
+
+        assert( ! success );
+        assert(output == 37);
+    }
 }
 
 // test graceful failure on values too large for uint
