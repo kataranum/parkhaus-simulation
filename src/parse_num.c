e6de0c4e802d0887ab650658a929a72cdeabd3b1
Kevin Müller <muellerk2006@gmail.com>
2026-03-12 08:47:28 +0100

    Implement parse_ulong

--- src/parse_num.c
+++ src/parse_num.c
@@ -5,7 +5,15 @@ bool parse_uint(const char *str, unsigned int *p_val) {
 }
 
 bool parse_ulong(const char *str, unsigned long *p_val) {
+    char *end;
+    long value = strtol(str, &end, 10);
 
+    if (str == end || value < 0) {
+        return false;
+    }
+
+    *p_val = (unsigned long) value;
+    return true;
 }
 
 bool parse_float(const char *str, float *p_val) {
