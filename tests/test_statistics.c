9f761c917fa987947f0b24a13e6050d89b7c002d
LuisDentler <luis.dentler06@gmail.com>
2026-03-13 16:46:22 +0000

    test init_statistics

--- tests/test_statistics.c
+++ tests/test_statistics.c
@@ -3,4 +3,22 @@
 #include "../inc/statistics.h"
 #include "../inc/car.h"
 
+//Test for init_statistics()
+void test_init_statistics()
+{
+    Statistics stats;
+    int init_result = init_statistics(&stats, "test_log.txt");
+
+    assert(init_result == 0);
+    assert(stats.sum_occupancy == 0);
+    assert(stats.sum_waiting_time == 0);
+    assert(stats.sum_queue_length == 0);
+    assert(stats.max_queue_length == 0);
+    assert(stats.full_occupancy_steps == 0);
+    assert(stats.finished_cars == 0);
+    assert(stats.log_file != NULL);
+
+    fclose(stats.log_file);
+}
+
 
