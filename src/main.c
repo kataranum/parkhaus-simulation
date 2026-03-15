686cda73c8973dc04180bf09f7cc0e07650ba3a9
Kevin Müller <muellerk2006@gmail.com>
2026-03-15 13:29:05 +0100

    Implement main.c

--- src/main.c
+++ src/main.c
@@ -20,5 +20,17 @@ RETURN 0
 ```
 */
 int main(void) {
-    // TODO
+    InputParams params = get_user_input();
+
+    Statistics stats;
+    if ( init_statistics(&stats, "output.txt") != 0) {
+        printf("Could not initialize statistics.\n");
+        return 1;
+    }
+
+    run_simulation(params, &stats);
+
+    output_total_statistics(&stats, params);
+
+    return 0;
 }
