50cc89391fcb291ef851888613ec4340ce80279b
LuisDentler <luis.dentler06@gmail.com>
2026-03-16 22:30:39 +0000

    Test for init_ test_parking_lot

--- tests/test_parking_lot.c
+++ tests/test_parking_lot.c
@@ -2,3 +2,13 @@
 #include <stdio.h>
 #include "../inc/parking_lot.h"
 
+// Test init_parking_lot 
+void test_init_parking_lot()
+{
+    ParkingLot lot = init_parking_lot(5);
+
+    assert(lot.length == 5);
+    assert(lot.p_array != NULL);
+
+    free_parking_lot(&lot);
+}
