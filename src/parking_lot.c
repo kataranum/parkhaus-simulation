7be8820c055fdd4402020efbfcd8377ef75b1dbc
Kevin Müller <muellerk2006@gmail.com>
2026-03-17 17:00:05 +0100

    Initialize car spaces in init_parking_lot

--- src/parking_lot.c
+++ src/parking_lot.c
@@ -5,13 +5,26 @@
 ParkingLot init_parking_lot(unsigned int length)
 {
     ParkingLot parking_lot;
-    parking_lot.p_array = malloc(length * sizeof(Car));
+    parking_lot.p_array = malloc(length * sizeof(parking_lot.p_array));
     if (parking_lot.p_array == NULL)
     {
         fprintf(stderr, "Error: Memory allocation failed for parking lot.\n");
         exit(EXIT_FAILURE);
     }
+
     parking_lot.length = length;
+
+    // initialize with empty cars
+    for (unsigned int i = 0; i < length; i++)
+    {
+        Car *p_car = parking_lot.p_array + i;
+
+        p_car->id = -1; // TODO: Do this per function
+        p_car->time_arrival_park = 0;
+        p_car->time_arrival_queue = 0;
+        p_car->time_park_duration = 0;
+    }
+
     return parking_lot;
 }
 
