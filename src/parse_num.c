8e166b3901745f304ed523d0d5429f4be031e8d5
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-03-17 14:16:54 +0100

    Change fromating in parse_num

--- src/parse_num.c
+++ src/parse_num.c
@@ -1,11 +1,13 @@
 #include <parse_num.h>
 
-bool parse_uint(const char *str, unsigned int *p_val) {
+bool parse_uint(const char *str, unsigned int *p_val)
+{
     unsigned long tmp = 0;
 
     bool success = parse_ulong(str, &tmp);
 
-    if (!success || tmp > INT_MAX) {
+    if (!success || tmp > INT_MAX)
+    {
         return false;
     }
 
@@ -13,11 +15,13 @@ bool parse_uint(const char *str, unsigned int *p_val) {
     return true;
 }
 
-bool parse_ulong(const char *str, unsigned long *p_val) {
+bool parse_ulong(const char *str, unsigned long *p_val)
+{
     char *end;
     long value = strtol(str, &end, 10);
 
-    if (str == end || value < 0) {
+    if (str == end || value < 0)
+    {
         return false;
     }
 
@@ -25,11 +29,13 @@ bool parse_ulong(const char *str, unsigned long *p_val) {
     return true;
 }
 
-bool parse_float(const char *str, float *p_val) {
+bool parse_float(const char *str, float *p_val)
+{
     char *end;
     float value = strtof(str, &end);
 
-    if (str == end) {
+    if (str == end)
+    {
         return false;
     }
 
