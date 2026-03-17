1b5befc63dbab84814cfee7f0a5e522fdd991500
Kevin Müller <muellerk2006@gmail.com>
2026-03-17 16:45:32 +0100

    Make car test_init pass
    This exposes an issue with another function (park_max_time = 1
    could be considered an invalid value for random_park_duration()).
    
    But it's not the purpose of test_init to test for that, so pick
    an "easier" value. Only the test that actually tests for invalid
    values should fail here.

--- tests/test_car.c
+++ tests/test_car.c
@@ -14,7 +14,7 @@ const int RNG_SEED_AMOUNT = sizeof(RNG_SEEDS) / sizeof(RNG_SEEDS[0]);
 void test_init(void) {
     // InputParams remain unused here, but are required for code to compile
     InputParams params;
-    params.park_max_time = 1;
+    params.park_max_time = 10;
 
     Car car = init_new_car(params);
 
