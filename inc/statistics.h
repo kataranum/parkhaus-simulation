cb37a9511ca672e172573d2d3ea53949373f8fdb
Kevin Müller <muellerk2006@gmail.com>
2026-03-17 19:11:04 +0100

    Adjust formatting in statistics.h

--- inc/statistics.h
+++ inc/statistics.h
@@ -2,6 +2,7 @@
 * File: statistics.h
 * Description: Interface for collecting, calculating and exporting
 *              statistical data of the parking garage simulation.
+
 * The statistics module is responsible only for data aggregation
 * and evaluation. It does not influence the simulation logic.
 */
@@ -14,13 +15,15 @@
 /*---------------------------------------------------------------*/
 /* Initialization                                                */
 /*---------------------------------------------------------------*/
+
 /**
  * @brief Initialize all statistic counters and accumulators
  *
  * This function must be called once before the simulation loop
  * starts. All internal values are reset to zero.
+ * 
+ * @param[out] stats Pointer to statistics struct
  * @param[in] filename Name der zu erstellenden Log-Datei.
- * @param[in] stats pointer auf Statistiks struct
  * @return int 0 bei Erfolg, -1 bei Dateifehler.
  */
 int init_statistics(Statistics *stats, const char *filename);
@@ -29,47 +32,52 @@ int init_statistics(Statistics *stats, const char *filename);
 /*---------------------------------------------------------------*/
 /* Time step based updates                                       */
 /*---------------------------------------------------------------*/
-/**
-* @brief Calculates and outputs the statistics for the current time step.
-*
-* This function is called exactly once per simulation time step.
-* It updates values related to occupancy, queue length and
-* full-capacity detection and outputs the statistic in the console.
-* 
-* @param simulation_data[in] simulation_data  
-* @param[in] stats pointer auf Statistiks struct
-*/
 
+/**
+ * @brief Calculates and outputs the statistics for the current time step.
+ *
+ * This function is called exactly once per simulation time step.
+ * It updates values related to occupancy, queue length and
+ * full-capacity detection and outputs the statistic in the console.
+ * 
+ * @param[in/out] stats Pointer to statistics struct
+ * @param[in] simulation_data Current simulation state
+ */
 void output_timestep_statistics(Statistics *stats, SimulationData simulation_data);
 
 /*---------------------------------------------------------------*/
 /* End statistics output                                         */
 /*---------------------------------------------------------------*/
+
 /**
  * @brief Outputs the summarized statistics at the end of the simulation.
- * @param params[in] given params 
- * @param[in] stats pointer auf Statistiks struct  
-**/
-
+ *
+ * @param[in] stats Pointer to statistics struct
+ * @param[in] params Input parameters
+ */
 void output_total_statistics(Statistics *stats, InputParams params);
 
 /*---------------------------------------------------------------*/
 /* Car leave                                                     */
 /*---------------------------------------------------------------*/
+
 /**
-* @brief Counts the finished_cars.
-* @param[in] stats pointer auf Statistiks struct
-**/
+ * @brief Counts the finished_cars.
+ * 
+ * @param[in/out] stats Pointer to statistics struct
+ */
 void statistics_car_leave(Statistics *stats);
 
 /*---------------------------------------------------------------*/
 /* Car arrive                                                    */
 /*---------------------------------------------------------------*/
+
 /**
-* @brief Calculates the waiting time of parked cars
-* @param[in] stats pointer auf Statistiks struct
-* @param[in] new_car car struct of parked car 
-*/
+ * @brief Calculates the waiting time of parked cars
+ *
+ * @param[in/out] stats Pointer to statistics struct
+ * @param[in] new_car car struct of parked car
+ */
 void statistics_car_arrive(Statistics *stats, Car new_car);
 
 #endif
