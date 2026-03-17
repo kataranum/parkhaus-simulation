8d559d5c5ca5c230964d130a40f364d828eaee59
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-03-17 14:15:11 +0100

    Remove pseudocode in parking_lot

--- src/parking_lot.c
+++ src/parking_lot.c
@@ -1,16 +1,5 @@
 #include "../inc/parking_lot.h"
 
-/*
-```PSEUDOCODE
-ParkingLot parking_lot;
-parking_lot.array = malloc(length * sizeof(Car));
-IF parking_lot.array == NULL:
-    EXIT with error code
-END IF
-parking_lot.length = length;
-RETURN parking_lot
-```
-*/
 ParkingLot init_parking_lot(unsigned int length);
 {
     ParkingLot parking_lot;
@@ -24,13 +13,6 @@ ParkingLot init_parking_lot(unsigned int length);
     return parking_lot;
 }
 
-/*
-```PSEUDOCODE
-free(parking_lot.array);
-parking_lot.array = NULL;
-parking_lot.length = 0;
-```
-*/
 void free_parking_lot(ParkingLot *p_parking_lot)
 {
     free(p_parking_lot->p_array);
@@ -38,20 +20,6 @@ void free_parking_lot(ParkingLot *p_parking_lot)
     p_parking_lot->length = 0;
 }
 
-/*
-```PSEUDOCODE
-int occupancy = 0;
-FOR i IN 0 TO parking_lot.length:
-    car = parking_lot.array[i];
-
-    IF !is_empty(car):
-        occupancy++;
-    END IF
-END FOR
-
-RETURN occupancy;
-```
-*/
 int get_occupancy(ParkingLot parking_lot)
 {
     int occupancy = 0;
@@ -66,19 +34,6 @@ int get_occupancy(ParkingLot parking_lot)
     return occupancy;
 }
 
-/*
-```PSEUDOCODE
-FOR i IN 0 TO parking_lot.length:
-    car = parking_lot.array[i];
-
-    IF is_empty(car):
-        RETURN True;
-    END IF
-END FOR
-
-RETURN False;
-```
-*/
 int room_available(ParkingLot parking_lot)
 {
     for (unsigned int i = 0; i < parking_lot.length; i++)
@@ -92,19 +47,6 @@ int room_available(ParkingLot parking_lot)
     return false;
 }
 
-/*
-```PSEUDOCODE
-FOR i IN 0 TO parking_lot.length:
-    car = parking_lot.array[i];
-
-    IF is_empty(car):
-        RETURN i;
-    END IF
-END FOR
-
-RETURN -1
-```
-*/
 int find_empty_space(ParkingLot parking_lot)
 {
     for (unsigned int i = 0; i < parking_lot.length; i++)
