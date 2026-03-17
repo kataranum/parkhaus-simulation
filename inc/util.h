bdb392579a79b49920e687ccfef6895664947768
Kevin Müller <muellerk2006@gmail.com>
2026-03-17 19:30:02 +0100

    Adjust documentation in util.h

--- inc/util.h
+++ inc/util.h
@@ -8,11 +8,11 @@
 #define UTIL_H
 
 /**
- * @brief Return a random float between 0.0 (inclusive) and 1.0 (exclusive)
+ * @brief Return a random float between 0.0 and 1.0
  * 
  * Uses `rand()` internally so uses the seed set by `srand()`
  *
- * @return float Random float between 0 and 1
+ * @return Random float between 0 and 1
  */
 float frand();
 
