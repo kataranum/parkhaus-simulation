fea4b6d1baf91c4a0dc29065d7dceb000ef16f2f
Bert <mandrossa.maxi-it25@it.dhbw-ravensburg.de>
2026-03-17 14:14:28 +0100

    Remove pseudocode in main

--- src/main.c
+++ src/main.c
@@ -2,28 +2,12 @@
 #include <statistics.h>
 #include <simulation.h>
 
-/*
-```PSEUDOCODE
-intput_params = get_user_input()
-
-Statistics stats;
-success = init_statistics(&stats, "output.txt")
-IF success != 0 THEN
-    PRINT "Error initializing statistics"
-    EXIT
-END IF
-
-run_simulation(input_params, &stats)
-
-output_total_statistics(&stats)
-RETURN 0
-```
-*/
 int main(void) {
     InputParams params = get_user_input();
 
     Statistics stats;
-    if ( init_statistics(&stats, "output.txt") != 0) {
+    if ( init_statistics(&stats, "output.txt") != 0)
+    {
         printf("Could not initialize statistics.\n");
         return 1;
     }
